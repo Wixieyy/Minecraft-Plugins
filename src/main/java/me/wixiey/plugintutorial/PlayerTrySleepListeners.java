85ec1db94440b99366fb1fed5d7a36252d531fa6
BuildTools <unconfigured@null.spigotmc.org>
2024-02-18 19:10:28 +0100

    fixed

--- src/main/java/me/wixiey/plugintutorial/PlayerTrySleepListeners.java
+++ src/main/java/me/wixiey/plugintutorial/PlayerTrySleepListeners.java
@@ -5,7 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
 
 public class PlayerTrySleepListeners implements Listener {
 
