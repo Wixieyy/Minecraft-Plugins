587be107cd9bb73f862688ec8c89cf8f4d591844
Wixiey <shendrickwilliams@gmail.com>
2024-02-18 19:16:27 +0100

    testing commit

--- src/main/java/me/wixiey/plugintutorial/PlayerTrySleepListeners.java
+++ src/main/java/me/wixiey/plugintutorial/PlayerTrySleepListeners.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-
 public class PlayerTrySleepListeners implements Listener {
 
     @EventHandler
