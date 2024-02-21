325e7e403f3a3d3a28b9a72792417ab51f702ef3
Wixiey <shendrickwilliams@gmail.com>
2024-02-21 21:36:00 +0100

    non func

--- src/main/java/me/wixiey/farmingplugin/Hoe3x3.java
+++ src/main/java/me/wixiey/farmingplugin/Hoe3x3.java
@@ -1,15 +1,27 @@
 package me.wixiey.farmingplugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 
 import java.awt.event.ItemEvent;
 
 public class Hoe3x3 implements Listener {
 
     @EventHandler
-    public void onHoeUsed (Entity event) {
-
+    public void onRightClick(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if(player.getInventory().getItemInMainHand().getType().equals(Material.IRON_HOE)) {
+            if(event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+                player.sendMessage("testing");
+            }
+        }
     }
-
 }
