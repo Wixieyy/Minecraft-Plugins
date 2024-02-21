5ed67efed64a258b8338b9fec2c7028ce1e47db4
Shendrick Williams <shendrickwilliams@gmail.com>
2024-02-21 21:39:47 +0100

    non func

--- src/main/java/me/wixiey/farmingplugin/Hoe3x3.java
+++ src/main/java/me/wixiey/farmingplugin/Hoe3x3.java
@@ -1,17 +1,11 @@
 package me.wixiey.farmingplugin;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.event.player.PlayerItemHeldEvent;
-
-import java.awt.event.ItemEvent;
 
 public class Hoe3x3 implements Listener {
 
